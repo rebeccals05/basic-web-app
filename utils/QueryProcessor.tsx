23ff0fbaaee5d7a1780cd3d0fbb282bc87b8832d
Rebecca Stamm <rstamm@andrew.cmu.edu>
2025-10-07 10:04:18 -0400

    change to name2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "rstamm";
+    return "rebecca";
   }
 
   return "";
